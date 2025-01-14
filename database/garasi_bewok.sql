07ec4d3b41206bf7e6a6c3122bb82492fcd0710d
ibnraihn <ibnuraihan004@gmail.com>
2025-01-15 00:36:21 +0700

    Add files via upload

---
+++ database/garasi_bewok.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Jan 2025 pada 18.35
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `garasi_bewok`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nomor_telepon` varchar(15) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `email`, `nomor_telepon`, `password`, `created_at`) VALUES
+(2, 'Rafi Rizqallah Andila', 'rafirizqallahandilla@gmail.com', '085711705767', '$2y$10$aNB3.9Hpd.7dmuS3czN3zOULUUldA3NVThxEotqNVHRfIC.kyi5iC', '2025-01-11 21:52:54'),
+(3, 'Ibnu Raihan', 'ibnuraihan@gmail.com', '08123456789', '$2y$10$fhZ061gx19Ln9Y7cBPmFuubHhvTi5jJ2I/odWWatFJUX0HW4c8Zi2', '2025-01-13 18:07:29'),
+(4, 'test', 'test@gmail.com', '08123456789', '$2y$10$M/RfuPP9Yvk6E9YlLpoEdeoRQL5MhfPtgDpHf0zee2us0IWw34LKK', '2025-01-13 20:09:15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
